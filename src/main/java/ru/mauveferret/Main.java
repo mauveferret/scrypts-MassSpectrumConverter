7bf5ae14f742065611d1885e702a0a0b74b569dc
mauveferret <mauveferret@gmail.com>
2020-05-01 14:26:48 +0300

    added massSpectrum builder

--- src/main/java/ru/mauveferret/Main.java
+++ src/main/java/ru/mauveferret/Main.java
@@ -1,5 +1,6 @@
 package ru.mauveferret;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -10,32 +11,49 @@ import java.util.*;
 public class Main  {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String currentJar = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         String currentDir = new File(currentJar).getParent();
-        System.out.println(currentDir);
-
+        System.out.print("Do you want to get time dependencies? If no, i'l make mass spectrum (Y/N or Д/Н) ");
+        String answer = scanner.nextLine();
+        boolean doTimeDependencies;
+        doTimeDependencies = answer.contains("Y") | answer.contains("y") | answer.contains("д") | answer.contains("н") ;
+        if (doTimeDependencies) System.out.println("Started converting to time dependencies...");
+        else
+            System.out.println("Started creating mass spectrum...");
         for (String filePaths: searching(new File(currentDir)))
         {
-            boolean iSConvertingSuccessed = convert(filePaths);
+            boolean iSConvertingSuccessed = convert(filePaths, doTimeDependencies);
             System.out.println(filePaths+" converting is"+ ((iSConvertingSuccessed)? " OK":" not OK"));
         }
         System.out.println("Converting is finished! Say bye :)");
-        Scanner scanner = new Scanner(System.in);
+
         scanner.nextLine();
         scanner.close();
     }
 
-    private static boolean convert(String filePath)
+    private static boolean convert(String filePath, boolean doTimeDependencies)
     {
 
         try {
-
-            //creating dir far all masses
-            String dir= new File(filePath).getParent()+File.separator+new File(filePath).getName().replace(".csv","");
+            //String timeLineForMassSpectrum = "AtomicNumber/time ";
+            String dir = "";
+            FileOutputStream spectrumWriter = new FileOutputStream(new File("shit"));
+            if (doTimeDependencies)
+            {
+                //creating dir far all masses
+                dir = new File(filePath).getParent() + File.separator + new File(filePath).getName().replace(".csv", "");
             //temp
-            System.out.println("dir is created: "+dir);
-            if (!new File(dir).mkdir() && !new File(dir).exists() ) throw new Exception("dir wasn't created");    //ha-ha
-            HashMap<Integer, FileOutputStream> massFiles = new HashMap<>();
+            System.out.println("dir is created: " + dir);
+            if (!new File(dir).mkdir() && !new File(dir).exists()) throw new Exception("dir wasn't created");    //ha-ha
+            }
+            else
+            {
+                String path = new File(filePath).getParent() + File.separator + new File(filePath).getName().replace(".csv", ".txt");
+                spectrumWriter = new FileOutputStream(new File(path));
+            }
+            HashMap<Double, FileOutputStream> massFiles = new HashMap<>();
+            HashMap<Double, Stack<String>> massSpectrum = new HashMap<>();
 
 
 
@@ -53,27 +71,55 @@ public class Main  {
                     String[] lineArray = line.split(",");
                     //what mass?
                     String mass = lineArray[1];
-                    mass = mass.substring(0, mass.indexOf("."));
-                    int massNumber = Integer.parseInt(mass.trim());
+                    //mass = mass.substring(0, mass.indexOf("."));
+                    double massNumber = Double.parseDouble(mass.trim());
 
-                    if (!massFiles.containsKey(massNumber))
-                    {
-                        File f = new File(dir+File.separator+new File(filePath).getName().
-                                replace(".csv","")+"_mass_"+massNumber+".txt");
-                        FileOutputStream writer = new FileOutputStream(f);
-                        massFiles.put(massNumber,writer);
+                    if (doTimeDependencies) {
+                        if (!massFiles.containsKey(massNumber)) {
+                            File f = new File(dir + File.separator + new File(filePath).getName().
+                                    replace(".csv", "") + "_mass_" + massNumber + ".txt");
+                            FileOutputStream writer = new FileOutputStream(f);
+                            massFiles.put(massNumber, writer);
+                        }
+                        //date?!
+                        int msPassed = convertDate(lineArray[0]);
+                        massFiles.get(massNumber).write((msPassed + " " + lineArray[2] + "\n").getBytes());
                     }
+                    else
+                    {
+                        //creating load for the operating memory, then will write to file
+                        if (!massSpectrum.containsKey(massNumber)) massSpectrum.put(massNumber, new Stack<String>());
+                        massSpectrum.get(massNumber).push(lineArray[2]);
+                        //timeLineForMassSpectrum+=( (int) convertDate(lineArray[0])/1000)+" ";
 
-                    //date?!
-                    int msPassed = convertDate(lineArray[0]);
-
-                    massFiles.get(massNumber).write((msPassed+" "+lineArray[2]+"\n").getBytes());
+                    }
                 }
                 if (line.contains("</ConfigurationData>")) configurationIsSkipped = true;
             }
 
-            //close all files
-            for (FileOutputStream f: massFiles.values()) f.close();
+            //closing streams, writing files to spectrumtable
+
+            if (doTimeDependencies)
+                for (FileOutputStream f: massFiles.values()) f.close(); //close all files
+            else
+            {
+                //spectrumWriter.write((timeLineForMassSpectrum+"\n").getBytes());
+                String lineToWrite;
+
+                SortedSet<Double> sortedMasses = new TreeSet();
+
+                for (double massNumber: massSpectrum.keySet()) sortedMasses.add(massNumber);
+
+                for (double massNumber: sortedMasses)
+                {
+                    lineToWrite = massNumber+" ";
+                    while (!massSpectrum.get(massNumber).isEmpty())
+                        lineToWrite+=massSpectrum.get(massNumber).pop()+" ";
+                    spectrumWriter.write((lineToWrite+"\n").getBytes());
+                }
+                spectrumWriter.close();
+            }
+
             return true;
         }
         catch (Exception e)
